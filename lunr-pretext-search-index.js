478b387c3cb98bd8e8768b8e4a136a573aa808df
Devin Diaz <diazdevin7@outlook.com>
2024-02-20 02:56:44 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -10,18 +10,18 @@ var ptx_lunr_docs = [
   "body": "  Devin's GitHub   copyright  "
 },
 {
-  "id": "section-1",
+  "id": "ch-1-section-1",
   "level": "1",
-  "url": "section-1.html",
+  "url": "ch-1-section-1.html",
   "type": "Section",
   "number": "1.1",
   "title": "Using latex and sageplot ._.",
   "body": " Using latex and sageplot ._.   Negative multiple of a curve   Plot of x^4 - 1 and its negative.      ma circle   (a circle of radius 3 centered at origin)     "
 },
 {
-  "id": "section-1-2",
+  "id": "ch-1-section-1-2",
   "level": "2",
-  "url": "section-1.html#section-1-2",
+  "url": "ch-1-section-1.html#ch-1-section-1-2",
   "type": "Figure",
   "number": "1.1.1",
   "title": "",
@@ -30,20 +30,38 @@ var ptx_lunr_docs = [
 {
   "id": "circle",
   "level": "2",
-  "url": "section-1.html#circle",
+  "url": "ch-1-section-1.html#circle",
   "type": "Figure",
   "number": "1.1.2",
   "title": "",
   "body": " ma circle   (a circle of radius 3 centered at origin)    "
 },
 {
-  "id": "section-2",
+  "id": "ch-1-section-2",
   "level": "1",
-  "url": "section-2.html",
+  "url": "ch-1-section-2.html",
   "type": "Section",
   "number": "1.2",
   "title": "Cool maths",
   "body": " Cool maths  Where my hate for math began   Where my sanity dropped   Thank the lord im a cs major and not math    I can't lie this is pretty cool, use show() to display the math format nicely   "
+},
+{
+  "id": "ch-2-section-1",
+  "level": "1",
+  "url": "ch-2-section-1.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "Python script attempt",
+  "body": " Python script attempt   This problem requires the user to write a Python program to calculate the product of two numbers.  Instructions:   Prompt the user to enter two numbers.  Calculate the product of the two numbers.  Display the product to the user.   Here is a code snippet as a reference:   Write a Python program to calculate the product of two numbers.  # Python program to calculate the product of two numbers # Input the first number num1 = float(input(\"Enter the first number: \")) # Input the second number num2 = float(input(\"Enter the second number: \")) # Calculate the product product = num1 * num2 # Display the product print(\"The product of\", num1, \"and\", num2, \"is\", product)   "
+},
+{
+  "id": "ch-2-section-1-2",
+  "level": "2",
+  "url": "ch-2-section-1.html#ch-2-section-1-2",
+  "type": "Checkpoint",
+  "number": "2.1.1",
+  "title": "",
+  "body": " This problem requires the user to write a Python program to calculate the product of two numbers.  Instructions:   Prompt the user to enter two numbers.  Calculate the product of the two numbers.  Display the product to the user.   Here is a code snippet as a reference:   Write a Python program to calculate the product of two numbers.  # Python program to calculate the product of two numbers # Input the first number num1 = float(input(\"Enter the first number: \")) # Input the second number num2 = float(input(\"Enter the second number: \")) # Calculate the product product = num1 * num2 # Display the product print(\"The product of\", num1, \"and\", num2, \"is\", product)  "
 },
 {
   "id": "backmatter-2",
